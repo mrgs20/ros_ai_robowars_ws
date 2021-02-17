dff9a42d868816db143162f9274506d257fc62d3
mrgs20 <marisaarivirta@gmail.com>
2021-02-17 13:00:47 +0200

    predicted_pose_publisher

--- src/robot_position/src/predict_position.py
+++ src/robot_position/src/predict_position.py
@@ -3,6 +3,7 @@
 import rospy, message_filters
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Range
+from geometry_msgs.msg import Pose
 import angles
 from tf.transformations import euler_from_quaternion
 import numpy as np
@@ -18,7 +19,7 @@ print(test_predictions)
 class Robot_position:
     def __init__(self):
         self.node = rospy.init_node("robot1_position_node")
-
+        self.predicted_pose_publisher = rospy.Publisher('robot1/predicted_pose', Pose, queue_size=1)
         self.us1_sub = message_filters.Subscriber('/ultrasonic1', Range)
         self.us2_sub = message_filters.Subscriber('/ultrasonic2', Range)
         self.us3_sub = message_filters.Subscriber('/ultrasonic3', Range)
@@ -29,9 +30,12 @@ class Robot_position:
 
         self.subs = message_filters.ApproximateTimeSynchronizer([self.us1_sub, self.us2_sub, self.us3_sub, self.us4_sub, self.us5_sub,
                                                                 self.us6_sub, self.odom_sub], queue_size=1, slop=0.9, allow_headerless=True)
-        self.subs.registerCallback(self.sensor_cb)
-        self.predict_data = np.zeros((1,7))
         self.model = keras.models.load_model('/home/mari/ros_ai_robowars_ws/saved_model/robot1_model490epochs')
+        self.predict_data = np.zeros((1,7))
+        self.subs.registerCallback(self.sensor_cb)
+
+        
+
 
     def sensor_cb(self, us1_sub, us2_sub, us3_sub, us4_sub, us5_sub, us6_sub, odom_sub):
         orientation_in_quaternions = (
@@ -47,7 +51,7 @@ class Robot_position:
         ground_truth_x = odom_sub.pose.pose.position.x
         ground_truth_y = odom_sub.pose.pose.position.y
 
-        self.predict_data[0][0] = yaw
+        self.predict_data[0][0] = yaw_radians
         self.predict_data[0][1] = us1_sub.range
         self.predict_data[0][2] = us2_sub.range
         self.predict_data[0][3] = us3_sub.range
@@ -55,9 +59,19 @@ class Robot_position:
         self.predict_data[0][5] = us5_sub.range
         self.predict_data[0][6] = us6_sub.range
 
-        test_predictions = model.predict(predict_data)
+        test_predictions = self.model.predict(self.predict_data)
         print("predictions  " + str(test_predictions[0][0]) + " " + str(test_predictions[0][1]))
         print("ground truth " + str(ground_truth_x) + " " + str(ground_truth_y))
+        msg = Pose()
+        msg.position.x = test_predictions[0][0]
+        msg.position.y = test_predictions[0][1]
+        msg.position.z = 0
+        msg.orientation.x = 0
+        msg.orientation.y = 0
+        msg.orientation.z = 0
+        msg.orientation.w = 0
+        self.predicted_pose_publisher.publish(msg)
+        
 
 if __name__ == '__main__':
     print("start")
